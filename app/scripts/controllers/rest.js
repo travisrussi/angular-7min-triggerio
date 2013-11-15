198b28f63fa5416265249515c7f5399103d38a91
Travis Russi <travis@russigroup.com>
2013-11-15 01:21:45 -0800

    fixed deleted animation name

--- app/scripts/controllers/rest.js
+++ app/scripts/controllers/rest.js
@@ -29,8 +29,7 @@ angular.module('$app')
     };
     $scope.skip = function() {
       audioFiles.play('skip');
-      $navigate.go($scope.next, '
-      '); //glueBottomFromTop
+      $navigate.go($scope.next, 'slideInFromRight'); //glueBottomFromTop
     };
 
   });
